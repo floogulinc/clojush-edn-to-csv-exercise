4bdd8af2e128bcd9503fdcc4865c8f1d6ffbe409
Paul Friederichsen <floogulinc@gmail.com>
2018-03-06 18:37:16 -0600

    Do the thing

--- src/edn2csv/core.clj
+++ src/edn2csv/core.clj
@@ -24,79 +24,89 @@
 (defn safe-println [output-stream & more]
   (.write output-stream (str (clojure.string/join "," more) "\n")))
 
+(def semantics-atom (atom {}))
+
+(defn new-uuid [] (str (java.util.UUID/randomUUID)))
+
+
+
 ; This prints out the relevant fields to the CSV filter
 ; and then returns 1 so we can count up how many individuals we processed.
 ; (The counting isn't strictly necessary, but it gives us something to
 ; fold together after we map this across the individuals; otherwise we'd
 ; just end up with a big list of nil's.)
 (defn print-individual-to-csv
-  [csv-file line]
-  (as-> line $
-    (map $ [:uuid :generation :location])
-    (concat $ ["Individual"])
-    (apply safe-println csv-file $))
-  1)
+    [individual-out-file
+     parentof-edges-out-file
+     {:keys [uuid generation location, parent-uuids,
+             genetic-operators, program, genome, total-error, errors], :as line}]
+    ;(as-> line $
+    ;      (map $ [:uuid :generation :location])
+    ;      (concat $ ["Individual"])
+    ;      (apply safe-println individual-out-file $))
+    (safe-println individual-out-file uuid generation location "Individual")
+    (swap! semantics-atom (partial merge-with concat) { {:total-error total-error :errors errors} [uuid] }) ; this feels wrong
+    (doseq [parent parent-uuids] (safe-println parentof-edges-out-file parent genetic-operators uuid "PARENT_OF"))
+    1)
 
-(defn edn->csv-sequential [edn-file csv-file]
-  (with-open [out-file (io/writer csv-file)]
-    (safe-println out-file individuals-header-line)
-    (->>
-      (line-seq (io/reader edn-file))
-      ; Skip the first line because it's not an individual
-      (drop 1)
-      (map (partial edn/read-string {:default individual-reader}))
-      (map (partial print-individual-to-csv out-file))
-      (reduce +)
-      )))
+(defn semantics-to-csv [semantics-out-file
+                        errors-out-file
+                        individual-semantics-edges-out-file
+                        semantics-error-edges-out-file
+                        [{total-error :total-error errors :errors} uuids]]
+    (let [semantic-uuid (new-uuid)] (safe-println semantics-out-file semantic-uuid total-error "Semantics")
+                                    (doseq [ind-uuid uuids] (safe-println individual-semantics-edges-out-file ind-uuid semantic-uuid "HAS_SEMANTICS"))
+                                    (doseq [[index item] (map-indexed vector errors)]
+                                        (let [error-uid (new-uuid)] (safe-println errors-out-file error-uid item index "Error")
+                                                                    (safe-println semantics-error-edges-out-file semantic-uuid error-uid "HAS_ERROR")))))
 
-(defn edn->csv-pmap [edn-file csv-file]
-  (with-open [out-file (io/writer csv-file)]
-    (safe-println out-file individuals-header-line)
-    (->>
-      (line-seq (io/reader edn-file))
-      ; Skip the first line because it's not an individual
-      (drop 1)
-      (pmap (fn [line]
-        (print-individual-to-csv out-file (edn/read-string {:default individual-reader} line))
-        1))
-      count
-      )))
 
-(defn edn->csv-reducers [edn-file csv-file]
-  (with-open [out-file (io/writer csv-file)]
-    (safe-println out-file individuals-header-line)
-    (->>
-      (iota/seq edn-file)
-      (r/map (partial edn/read-string {:default individual-reader}))
-      ; This eliminates empty (nil) lines, which result whenever
-      ; a line isn't a 'clojush/individual. That only happens on
-      ; the first line, which is a 'clojush/run, but we still need
-      ; to catch it. We could do that with `r/drop`, but that
-      ; totally kills the parallelism. :-(
-      (r/filter identity)
-      (r/map (partial print-individual-to-csv out-file))
-      (r/fold +)
-      )))
+(defn build-csv-filename
+    [edn-filename type]
+    (str (fs/parent edn-filename)
+         "/"
+         (fs/base-name edn-filename ".edn")
+         "_"
+         type
+         ".csv"))
 
-(defn build-individual-csv-filename
-  [edn-filename strategy]
-  (str (fs/parent edn-filename)
-       "/"
-       (fs/base-name edn-filename ".edn")
-       (if strategy
-         (str "_" strategy)
-         "_sequential")
-       "_Individuals.csv"))
+(defn edn->csv-reducers [edn-file]
+    (with-open [individual-out-file (io/writer (build-csv-filename edn-file "Individual"))
+                semantics-out-file (io/writer (build-csv-filename edn-file "Semantics"))
+                errors-out-file (io/writer (build-csv-filename edn-file "Errors"))
+                parentof-edges-out-file (io/writer (build-csv-filename edn-file "ParentOf_edges"))
+                individual-semantics-edges-out-file (io/writer (build-csv-filename edn-file "Individual_Semantics_edges"))
+                semantics-error-edges-out-file (io/writer (build-csv-filename edn-file "Semantics_Error_edges"))]
+        (safe-println individual-out-file "UUID:ID(Individual),Generation:int,Location:int,:LABEL")
+        (safe-println semantics-out-file "UUID:ID(Semantics),TotalError:int,:LABEL")
+        (safe-println errors-out-file "UUID:ID(Error),ErrorValue:int,Location:int,:LABEL")
+        (safe-println parentof-edges-out-file ":START_ID(Individual),GeneticOperator,:END_ID(Individual),:TYPE")
+        (safe-println individual-semantics-edges-out-file ":START_ID(Individual),:END_ID(Semantics),:TYPE")
+        (safe-println semantics-error-edges-out-file "START_ID(Semantics),:END_ID(Error),:TYPE")
+        (->>
+            (iota/seq edn-file)
+            (r/map (partial edn/read-string {:default individual-reader}))
+            ; This eliminates empty (nil) lines, which result whenever
+            ; a line isn't a 'clojush/individual. That only happens on
+            ; the first line, which is a 'clojush/run, but we still need
+            ; to catch it. We could do that with `r/drop`, but that
+            ; totally kills the parallelism. :-(
+            (r/filter identity)
+            (r/map (partial print-individual-to-csv
+                            individual-out-file
+                            parentof-edges-out-file))
+            (r/fold +)
+            )
+        (doall (pmap (partial semantics-to-csv semantics-out-file ; this part is short and r/map doesn't work. Very little difference between map and pmap as well.
+                        errors-out-file
+                        individual-semantics-edges-out-file
+                        semantics-error-edges-out-file)
+                        @semantics-atom))))
 
 (defn -main
-  [edn-filename & [strategy]]
-  (let [individual-csv-file (build-individual-csv-filename edn-filename strategy)]
+  [edn-filename]
     (time
-      (condp = strategy
-        "sequential" (edn->csv-sequential edn-filename individual-csv-file)
-        "pmap" (edn->csv-pmap edn-filename individual-csv-file)
-        "reducers" (edn->csv-reducers edn-filename individual-csv-file)
-        (edn->csv-sequential edn-filename individual-csv-file))))
+         (edn->csv-reducers edn-filename))
   ; Necessary to get threads spun up by `pmap` to shutdown so you get
   ; your prompt back right away when using `lein run`.
   (shutdown-agents))
